18457b2038ded70a6f894ca4856f99ad839322ef
Mor'du D2 <psillithid@gmail.com>
2021-02-21 15:26:20 +1100

    Add SocialConnections/Discord to user

--- user.go
+++ user.go
@@ -9,27 +9,28 @@ type User struct {
 	Type       string `json:"type"`
 	ID         string `json:"id"`
 	Attributes struct {
-		FirstName       string   `json:"first_name"`
-		LastName        string   `json:"last_name"`
-		FullName        string   `json:"full_name"`
-		Vanity          string   `json:"vanity"`
-		Email           string   `json:"email"`
-		About           string   `json:"about"`
-		FacebookId      string   `json:"facebook_id"`
-		Gender          int      `json:"gender"`
-		HasPassword     bool     `json:"has_password"`
-		ImageURL        string   `json:"image_url"`
-		ThumbURL        string   `json:"thumb_url"`
-		YouTube         string   `json:"youtube"`
-		Twitter         string   `json:"twitter"`
-		Facebook        string   `json:"facebook"`
-		IsEmailVerified bool     `json:"is_email_verified"`
-		IsSuspended     bool     `json:"is_suspended"`
-		IsDeleted       bool     `json:"is_deleted"`
-		IsNuked         bool     `json:"is_nuked"`
-		Created         NullTime `json:"created"`
-		URL             string   `json:"url"`
-		DiscordId	string 	 `json:"discord_id"`
+		FirstName         string            `json:"first_name"`
+		LastName          string            `json:"last_name"`
+		FullName          string            `json:"full_name"`
+		Vanity            string            `json:"vanity"`
+		Email             string            `json:"email"`
+		About             string            `json:"about"`
+		FacebookId        string            `json:"facebook_id"`
+		Gender            int               `json:"gender"`
+		HasPassword       bool              `json:"has_password"`
+		ImageURL          string            `json:"image_url"`
+		ThumbURL          string            `json:"thumb_url"`
+		YouTube           string            `json:"youtube"`
+		Twitter           string            `json:"twitter"`
+		Facebook          string            `json:"facebook"`
+		IsEmailVerified   bool              `json:"is_email_verified"`
+		IsSuspended       bool              `json:"is_suspended"`
+		IsDeleted         bool              `json:"is_deleted"`
+		IsNuked           bool              `json:"is_nuked"`
+		Created           NullTime          `json:"created"`
+		URL               string            `json:"url"`
+		DiscordId         string            `json:"discord_id"`
+		SocialConnections SocialConnections `json:"social_connections"`
 	} `json:"attributes"`
 	Relationships struct {
 		Pledges *PledgesRelationship `json:"pledges,omitempty"`
@@ -43,4 +44,13 @@ type UserResponse struct {
 	Links    struct {
 		Self string `json:"self"`
 	} `json:"links"`
+}
+
+type SocialConnections struct {
+	Discord DiscordConnection `json:"discord"`
+}
+
+type DiscordConnection struct {
+	Url    string `json:"url"`
+	UserID string `json:"user_id"`
 }
